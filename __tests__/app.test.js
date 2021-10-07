efa480592a5b907e7db2c47679cdd76a59e000b6
Simon Kaine <simonkaine@gmail.com>
2021-10-07 14:04:20 -0700

    red failing test for POST route to /signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,17 @@ describe('authentication-04 routes', () => {
     return setup(pool);
   });
 
+  // const signUpData = { email: 'bishop@kaine.com', password: 'dearlordbabyjesus' };
+
+  it('POST route to /signup that responds with a newly created Users id', async () => {
+    const res = await request(app).post('/api/v1/signup').send({ email: 'bishop@kaine.com', password: 'dearlordbabyjesus' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'bishop@kaine.com',
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
